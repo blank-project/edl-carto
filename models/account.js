a856a4ff04db7109b4dc3ebbb7501e598e6fd705
Yohann Gablowski <yohann.gablowski@bellevillecitoyenne.fr>
2017-10-11 11:30:10 +0200

    updated model

--- models/account.js
+++ models/account.js
@@ -6,7 +6,21 @@ var Account = new Schema({
     username: String,
     password: String,
     email: String,
-    admin: Boolean
+    admin: Boolean,
+    structureName: String,
+    structureMail: String,
+    structurePhone: Number,
+    adressNb: Number,
+    adressType: String,
+    adressName: String,
+    adressZip: Number,
+    metro: String,
+    website: String,
+    type: Array,
+    public: String,
+    time: String,
+    meeting: Boolean
+
 });
 
 Account.plugin(passportLocalMongoose);
