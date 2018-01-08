81ef6bf4f0a121519a22a82581a89d193fe04968
Yohann Gablowski <yohann.gablowski@bellevillecitoyenne.fr>
2018-01-08 14:31:26 +0100

    sdfq

--- routes/index.js
+++ routes/index.js
@@ -401,6 +401,7 @@ doc.moveTo(40, 160)   // lignes horizontales tableau première page
       var pageNb = Math.ceil((result.length-3)/4);
 
       for(var i=0;i<3;i++) {
+        console.log(result[i]);
         if(!result[i].admin) {
         doc.fontSize(14)
            .text(result[i].structureName, 40, 200+259*i, {width:130, align: 'center'});
