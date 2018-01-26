7b72a547cac241a584e2a90e4e6abbcc7c5a617e
Marin Procureur <marin.procureur@gmail.com>
2018-01-26 17:00:44 +0100

    draft v2

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,14 @@
-  var checkbox = document.getElementById("permCheck2");
-  var form = document.getElementById("perm2");
 
-  checkbox.addEventListener("click", function() {
 
-    console.log(form.style.visibility);
+/* print global pdf or page if on /map */
 
-    if(form.style.visibility = "hidden") {
-      form.style.visibility= "visible";
-    } else {
-      form.style.visibility= "hidden";
-    }
-
-  }, true);
+var button = document.getElementById('print');
+console.log(button);
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (window.location.pathname === "/map") {
+    window.print();
+  } else {
+    window.open(e.target.href);
+  }
+});
