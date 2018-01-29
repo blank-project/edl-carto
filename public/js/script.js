981b8ae7974ac4883791768028975df3ad03d2e1
Marin Procureur <marin.procureur@gmail.com>
2018-01-29 17:54:26 +0100

    add loader & auto print for link from frontpage

--- public/js/script.js
+++ public/js/script.js
@@ -3,12 +3,21 @@
 /* print global pdf or page if on /map */
 
 var button = document.getElementById('print');
-console.log(button);
 button.addEventListener('click', function(e) {
   e.preventDefault();
   if (window.location.pathname === "/map") {
     window.print();
   } else {
-    window.open(e.target.href);
+    window.open(window.location.origin + "/map?print");
   }
 });
+
+/* automated print */
+if (window.location.pathname === "/map" && window.location.search.indexOf("print") > -1) {
+  var interval = setTimeout(function() {
+    document.getElementById('loader').classList.add('hide');
+    window.print();
+  }, 5000);
+} else {
+  document.getElementById('loader').classList.add('hide');
+}
