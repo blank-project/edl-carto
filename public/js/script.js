8c458d78be01e2d0b303af2f3e322baddf761142
Marin Procureur <marin.procureur@gmail.com>
2018-03-16 14:06:07 +0100

    Add script to toggle perm hors les murs form part

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,17 @@
 /* print global pdf or page if on /map */
 var button = document.getElementById('print');
-button.addEventListener('click', function(e) {
-  e.preventDefault();
-  if (window.location.pathname === "/map") {
-    // print now
-    window.print();
-  } else {
-    // open new page with print param to force print
-    window.open(window.location.origin + "/map?print");
-  }
-});
+if (button) {
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (window.location.pathname === "/map") {
+      // print now
+      window.print();
+    } else {
+      // open new page with print param to force print
+      window.open(window.location.origin + "/map?print");
+    }
+  });
+}
 
 /* automated print */
 if (window.location.pathname === "/map" && window.location.search.indexOf("print") > -1) {
@@ -20,7 +22,25 @@ if (window.location.pathname === "/map" && window.location.search.indexOf("print
     // print the page
     window.print();
   }, 5000);
-} else {
-  // hide loader
+} else if (document.getElementById('loader')) {
   document.getElementById('loader').classList.add('hide');
+}
+
+/* script to show 2nd part of form for "hors les murs" */
+var perm2 = document.getElementById('perm2');
+if (perm2) {
+  var checkbox = document.querySelector('input[name="perm2"]');
+  toggleShowForm(checkbox, perm2);
+  checkbox.addEventListener('change', function(e) {
+    toggleShowForm(e.target, perm2);
+  });
+}
+
+/* toggle showing of 2nd part form */
+function toggleShowForm(checkbox, form) {
+  if (checkbox.checked) {
+    form.classList.add('show');
+  } else {
+    form.classList.remove('show');
+  }
 }
